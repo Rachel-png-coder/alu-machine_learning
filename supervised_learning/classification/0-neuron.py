630c5845c2b7230dd9a78d1e976694a0464d8e65
Rachel <r.toronga@alustudent.com>
2026-05-13 01:02:03 -0700

    0-neuron.py

--- supervised_learning/classification/0-neuron.py
+++ supervised_learning/classification/0-neuron.py
@@ -1,15 +1,39 @@
 #!/usr/bin/env python3
+"""
+Neural Class
+"""
+
 
 import numpy as np
 
-NN = __import__("12-neural_network").NeuralNetwork
 
-lib_train = np.load("../data/Binary_Train.npz")
-X_3D, Y = lib_train["X"], lib_train["Y"]
-X = X_3D.reshape((X_3D.shape[0], -1)).T
+class Neuron:
+    """
+    Neuron class represents a single neuron in a neural network.
 
-np.random.seed(0)
-nn = NN(X.shape[0], 3)
-A, cost = nn.evaluate(X, Y)
-print(A)
-print(cost
+    Attributes:
+        nx (int): The number of input features.
+        W (ndarray): The weights vector for the neuron.
+        b (float): The bias for the neuron.
+        A (float): The activated output of the neuron (forward propagation).
+    """
+
+    def __init__(self, nx):
+        """
+        Initializes a neuron.
+
+        Args:
+            nx (int): The number of input features.
+
+        Raises:
+            TypeError: If nx is not an integer.
+            ValueError: If nx is not a positive integer.
+        """
+        if type(nx) != int:
+            raise TypeError('nx must be an integer')
+        if nx < 1:
+            raise ValueError('nx must be a positive integer')
+        self.nx = nx
+        self.W = np.random.randn(1, nx)
+        self.b = 0
+        self.A = 0
