3acaf7f373a6ef0e87d0dc01d81c3f2933442519
Rachel <r.toronga@alustudent.com>
2026-05-13 01:12:28 -0700

    1-neuron.py

---
+++ supervised_learning/classification/1-neuron.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+"""
+Neural Class
+"""
+
+
+import numpy as np
+
+
+class Neuron:
+    """
+    Neuron class represents a single neuron in a neural network.
+
+    Attributes:
+        nx (int): The number of input features.
+        W (ndarray): The weights vector for the neuron.
+        b (float): The bias for the neuron.
+        A (float): The activated output of the neuron (forward propagation).
+    """
+
+    def __init__(self, nx):
+        """
+        Initializes a neuron.
+
+        Args:
+            nx (int): The number of input features.
+
+        Raises:
+            TypeError: If nx is not an integer.
+            ValueError: If nx is not a positive integer.
+        """
+        if type(nx) != int:
+            raise TypeError('nx must be an integer')
+        if nx < 1:
+            raise ValueError('nx must be a positive integer')
+        self.nx = nx
+        self.__W = np.random.normal(size=(1, nx))
+        self.__b = 0
+        self.__A = 0
+
+    @property
+    def W(self):
+        return self.__W
+
+    @property
+    def b(self):
+        return self.__b
+
+    @property
+    def A(self):
+        return self.__A
